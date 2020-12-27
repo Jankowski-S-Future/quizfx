61bbc4cbad45ad7a0f3f4515a3b417405dcb9eff
Jankowski-S-Future <jankos281292@gmail.com>
2020-12-27 14:24:16 +0100

    add comments and improve readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# quizfx
+# QuizFX
+
+Gra memory napisana w języku Java, z wykorzystaniem pakietu JavaFX.
+
+## Opis
+
+Znajdź wszystkie pary symboli marek samochodowych, wykonując jak najmniej kliknięć.
+
+## Klasy JavaFX wykorzystane w projekcie
+
+```
+javafx.application.Application;
+javafx.stage.Stage;
+javafx.scene.Scene;
+javafx.scene.control.Alert;
+javafx.scene.control.Button;
+javafx.scene.image.Image;
+javafx.scene.image.ImageView;
+javafx.scene.layout.GridPane;
+```
--- src/application/Main.java
+++ src/application/Main.java
@@ -6,13 +6,17 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	private ViewBuilder builder = new ViewBuilder();
+	// Utworzenie instancji obiektu do budowania widoku
+	private ViewHelper helper = new ViewHelper();
 
+	// Implementacja metody abstrakcyjnej start() z klasy Application pakietu javafx
+	// Startuje aplikacjê
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("Dopasuj Logo Producenta Samochodu");
 
-		GridPane grid = builder.buildGridPane();
-		Scene scene = builder.buildScene(grid);
+		// Utworzenie widoku
+		GridPane grid = helper.buildGridPane();
+		Scene scene = helper.buildScene(grid);
 
 		primaryStage.setScene(scene);
 		primaryStage.setResizable(false);
